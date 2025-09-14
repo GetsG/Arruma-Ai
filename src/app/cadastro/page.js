96528fcb464da126e63518ef144d6b78496884cc
GetsG <getuliogo65@gmail.com>
2025-09-14 11:54:01 -0300

    Adicionar telas de login e cadastro

---
+++ src/app/cadastro/page.js
@@ -0,0 +1,13 @@
+import Header from '@/componentes/Header/Header'
+import Cadastro from '@/componentes/Login/Cadastro'
+import estilos from './cadastro.module.css'
+
+export default function Teste() {
+  return (
+    <div className={estilos.container}>
+      <Header />
+      <Cadastro />
+    </div>
+
+);
+}
---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,9 @@
+import estilos from "./Header.module.css"
+
+export default function Header(){
+    return(
+        <header className={estilos.component_header}>
+            <h1>Arruma - Ai</h1>
+        </header>
+    )
+}
